6589148fccc8de7c05b7ac2876867c986bf58317
Ronn2227 <ronn2227@hotmail.com>
2015-11-04 11:41:12 -0600

    Búsqueda exhaustiva
    implementación del método de busqueda exhaustiva

--- Tarea 3 estructuras/Tarea 3 estructuras/Parque.h
+++ Tarea 3 estructuras/Tarea 3 estructuras/Parque.h
@@ -7,23 +7,42 @@ public:
 	Parque(){};
 	~Parque(){}
 	std::vector<int> busquedaExhaustiva(const int** espera, int m, int n, const int* disfrute, const int** traslado){
+		maximo = 0;
+		std::vector<int> respuesta(m);
+		std::vector<int> borrador(m);
+		busquedaRecursiva();
+		respuesta.shrink_to_fit();
+
 		return respuesta;
 	}
 	std::vector<int> programacionDinamica(const int** espera, int m, int n, const int* disfrute, const int** traslado){};
 	std::vector<int> algoritmoAvido(const int** espera, int m, int n, const int* disfrute, const int** traslado){};
 
 private:
-	std::vector<int> respuesta;
-	std::vector<int> borrador (4);
+	int maximo = 0;
 
-	int busquedaRecursiva(int i, int tiempoRestante, int DisfruteAcumulado, const int ** espera, const int** traslado, const int* disfrute, int m, int n){
+	int busquedaRecursiva(int i, int tiempoAcumulado , int DisfruteAcumulado, const int ** espera, const int** traslado,
+						  const int* disfrute, int m, int n, std::vector<int> respuesta, std::vector<int> borrador){
 		int maxDisfrute = 0;
-		if (tiempoRestante == 0){
+		if (tiempoAcumulado == m || i == m){
 			maxDisfrute = DisfruteAcumulado;
 		}
 		else{
-			for (int j = 0; j < n; ++j){
-				borador[j] = j;
+			for (int j = 1; j <= n; ++j){
+				int duracion = espera[j][tiempoAcumulado] + traslado[borrador[i-1]][j] + disfrute[j];
+				if (i = 0){
+					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, DisfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
+					if (maxDisfrute > maximo){
+						maximo = maxDisfrute;
+						respuesta = borrador;
+				}
+				else if (j != borrador[i - 1] && duracion + traslado[j][n] <= m - tiempoAcumulado){
+					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, DisfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
+					if (maxDisfrute > maximo){
+						maximo = maxDisfrute;
+						respuesta = borrador;
+					}
+				}
 			}
 		}
 		return maxDisfrute;

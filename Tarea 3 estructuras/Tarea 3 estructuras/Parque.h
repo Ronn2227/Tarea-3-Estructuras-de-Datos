de270161fc091cd5104a139e51b295e23746dcff
Ronn2227 <ronn2227@hotmail.com>
2015-11-05 23:34:26 -0600

    prueba archivos
    Prueba de lectura de archivos para las matrices

--- Tarea 3 estructuras/Tarea 3 estructuras/Parque.h
+++ Tarea 3 estructuras/Tarea 3 estructuras/Parque.h
@@ -10,7 +10,7 @@ public:
 		maximo = 0;
 		std::vector<int> respuesta(m);
 		std::vector<int> borrador(m);
-		busquedaRecursiva();
+		busquedaRecursiva(0, 0 , 0, espera, traslado, disfrute, m, n, respuesta, borrador);
 		respuesta.shrink_to_fit();
 
 		return respuesta;
@@ -21,29 +21,39 @@ public:
 private:
 	int maximo = 0;
 
-	int busquedaRecursiva(int i, int tiempoAcumulado , int DisfruteAcumulado, const int ** espera, const int** traslado,
+	int busquedaRecursiva(int i, int tiempoAcumulado , int disfruteAcumulado, const int ** espera, const int** traslado,
 						  const int* disfrute, int m, int n, std::vector<int> respuesta, std::vector<int> borrador){
 		int maxDisfrute = 0;
 		if (tiempoAcumulado == m || i == m){
-			maxDisfrute = DisfruteAcumulado;
+			maxDisfrute = disfruteAcumulado;
 		}
 		else{
 			for (int j = 1; j <= n; ++j){
-				int duracion = espera[j][tiempoAcumulado] + traslado[borrador[i-1]][j] + disfrute[j];
+				int duracion;
+				if (i > 0){
+					duracion = espera[j][tiempoAcumulado] + traslado[borrador[i - 1]][j] + disfrute[j];
+				}
+
 				if (i = 0){
-					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, DisfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
+					duracion = espera[j][tiempoAcumulado] + traslado[borrador[i]][j] + disfrute[j];
+					borrador[i] = j;
+					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, disfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
 					if (maxDisfrute > maximo){
 						maximo = maxDisfrute;
 						respuesta = borrador;
+					}
 				}
 				else if (j != borrador[i - 1] && duracion + traslado[j][n] <= m - tiempoAcumulado){
-					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, DisfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
+					maxDisfrute = busquedaRecursiva(i + 1, tiempoAcumulado + duracion, disfruteAcumulado + disfrute[j], espera, traslado, disfrute, m, n, respuesta, borrador);
 					if (maxDisfrute > maximo){
 						maximo = maxDisfrute;
 						respuesta = borrador;
 					}
 				}
 			}
+		}
+		if (maxDisfrute == 0){
+			maxDisfrute = disfruteAcumulado;
 		}
 		return maxDisfrute;
 	}
--- Tarea 3 estructuras/Tarea 3 estructuras/Tarea 3 estructuras.cpp
+++ Tarea 3 estructuras/Tarea 3 estructuras/Tarea 3 estructuras.cpp
@@ -2,10 +2,75 @@
 //
 
 #include "stdafx.h"
+#include "Parque.h"
+//#include <fstream>
+//#include <iostream>
+using namespace std;
 
+int _tmain(int argc, _TCHAR* argv[]){
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+	int i = 0;
+	int j = 0;
+	int * disfrute[13];
+	int ** espera = new int*[14];
+	int ** traslado = new int*[14];
+	int ** tiempo = new int*[14];
+	string linea;
+	string data;
+	ifstream fileDisfrute;
+	ifstream fileEspera;
+	ifstream fileTraslado;
+	fileDisfrute.open("disfrute.csv");
+	fileEspera.open("espera.csv");
+	fileTraslado.open("traslado.csv");
+
+	if (fileDisfrute.is_open()){ // Lee el archivo y crea la lista de operaciones.
+		while (getline(fileDisfrute, linea)){
+			while (linea.find(",")!= string::npos){
+				int a = linea.find(",");
+				int b;
+				data = linea.substr(0, a);
+				linea = linea.substr(a + 1);
+				try{
+					if (linea.length() == 0){
+						b = -1;
+					}
+					else{
+						b = stoi(linea);
+					}
+				}
+				catch (invalid_argument exception){
+					b = -2;
+				}
+				if (b > -2){
+					
+					disfrute[i] = &b;
+					++i;
+				}
+			}	
+		}
+		fileDisfrute.close();
+	}
+
+	for (j = 0; j <= i; ++j){
+		cout << *disfrute[j] << endl;
+	}
+	/*
+	if (fileEspera.is_open()){ // Lee el archivo y crea la lista de operaciones.
+		while (getline(fileEspera, linea)){
+
+		}
+		fileEspera.close();
+	}
+
+	if (fileTraslado.is_open()){ // Lee el archivo y crea la lista de operaciones.
+		while (getline(fileTraslado, linea)){
+
+		}
+		fileTraslado.close();
+	}
+	*/
+
 	return 0;
 }
 
